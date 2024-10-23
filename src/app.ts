084b38f360b7536276e6b1ec2c0e29286113662b
Paul McClean <pmcclean@petsafe.net>
2024-10-23 11:05:48 +0100

    Improve message

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import { add } from "./calculator";
 
-console.log("Hello World, this is your app speaking!");
+console.log("Made with ❤️ in Dundalk 🇮🇪");
 console.log("Calculated the sum 1 + 2 = ", add(1, 2));
